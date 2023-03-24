9bbdfdc5697f5b3175d4c2d1fea9b85c02c60fb6
Hoang Huynh Tuan Kiet <hoanghuynhtuankiet69@gmail.com>
2023-03-24 19:56:37 +0700

    Add project files.

---
+++ rename-method/afterrename/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace renamemethod.afterrename
+{
+	public class Customer
+	{
+		private string _firstName;
+		private string _lastName;
+		private int _age;
+
+		public Customer(string firstName, string lastName, int age)
+		{
+			_firstName = firstName;
+			_lastName = lastName;
+			_age = age;
+		}
+
+		public string GetFirstName() { return _firstName; }
+		public string GetLastName() { return _lastName; }
+		public int GetAge() { return _age; }
+	}
+}
---
+++ rename-method/beforerename/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace renamemethod.beforerename
+{
+	public class Customer
+	{
+		private string _firstName;
+		private string _lastName;
+		private int _age;
+
+		public Customer(string firstName, string lastName, int age)
+		{
+			_firstName = firstName;
+			_lastName = lastName;
+			_age = age;
+		}
+
+		public string GetFn() { return _firstName; }
+		public string GetLn() { return _lastName; }
+		public int GetA() { return _age; }
+	}
+}
