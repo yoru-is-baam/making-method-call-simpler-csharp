adb0e6442af74ba39712fb8904812d668be00d9d
Hoang Huynh Tuan Kiet <hoanghuynhtuankiet69@gmail.com>
2023-03-29 22:40:31 +0700

    Add remove parameter project

---
+++ remove-parameter/afterremove/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace remove_parameter.afterremove
+{
+	internal class Customer
+	{
+		private string _firstName;
+		private string _lastName;
+
+		public Customer(string firstName, string lastName)
+		{
+			_firstName = firstName;
+			_lastName = lastName;
+		}
+
+		public string GetFirstName() { return _firstName; }
+		public string GetLastName() { return _lastName; }
+	}
+}
---
+++ remove-parameter/beforeremove/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace remove_parameter.beforeremove
+{
+	internal class Customer
+	{
+		private string _firstName;
+		private string _lastName;
+
+		public Customer(string firstName, string lastName)
+		{
+			_firstName = firstName;
+			_lastName = lastName;
+		}
+
+		public string GetFirstName(int age) { return _firstName; }
+		public string GetLastName() { return _lastName; }
+	}
+}
