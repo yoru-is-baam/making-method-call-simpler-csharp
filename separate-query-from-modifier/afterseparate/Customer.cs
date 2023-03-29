16a52aad2d8ba692804458b6aee0ebd3e02a5f19
Hoang Huynh Tuan Kiet <hoanghuynhtuankiet69@gmail.com>
2023-03-30 00:21:43 +0700

    add separate query from modifier project

---
+++ separate-query-from-modifier/afterseparate/Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace separate_query_from_modifier.afterseparate
+{
+	internal class Customer
+	{
+		private string _name;
+		private int _age;
+
+		public Customer(string name, int age)
+		{
+			_name = name;
+			_age = age;
+		}
+
+		public String GetName() { return _name; }
+
+		public int GetAge()
+		{
+			return _age;
+		}
+
+		public void IncrementAge()
+		{
+			_age++;
+		}
+	}
+}
---
+++ separate-query-from-modifier/beforeseparate/Customer.cs
@@ -0,0 +1,23 @@
+﻿namespace separate_query_from_modifier.beforeseparate
+{
+	internal class Customer
+	{
+		private string _name;
+		private int _age;
+
+		public Customer(string name, int age)
+		{
+			_name = name;
+			_age = age;
+		}
+
+		public String GetName() { return _name; }
+
+		public int GetAgeAndIncrement() {
+			int result = _age;
+			_age++;
+
+			return result; 
+		}
+	}
+}
