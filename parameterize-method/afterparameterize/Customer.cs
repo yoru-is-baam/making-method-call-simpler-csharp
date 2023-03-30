33627691dc265ff6d6403fd54cbca9e36902204e
Hoang Huynh Tuan Kiet <hoanghuynhtuankiet69@gmail.com>
2023-03-30 10:04:28 +0700

    Add parameterize method project

---
+++ parameterize-method/afterparameterize/Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace parameterize_method.afterparameterize
+{
+	internal class Customer
+	{
+		private string _name;
+		private int _age;
+
+		public Customer(string name, int age)
+		{
+			_name = name;
+			_age = age;
+		}
+
+		public String GetName() { return _name; }
+
+		public int GetAge()
+		{
+			return _age;
+		}
+
+		public void IncrementAge(int value)
+		{
+			_age += value;
+		}
+	}
+}
---
+++ parameterize-method/beforeparameterize/Customer.cs
@@ -0,0 +1,31 @@
+﻿namespace parameterize_method.beforeparameterize
+{
+	internal class Customer
+	{
+		private string _name;
+		private int _age;
+
+		public Customer(string name, int age)
+		{
+			_name = name;
+			_age = age;
+		}
+
+		public String GetName() { return _name; }
+
+		public int GetAge()
+		{
+			return _age;
+		}
+
+		public void IncrementAgeToOne()
+		{
+			_age++;
+		}
+
+		public void IncrementAgeToFive()
+		{
+			_age += 5;
+		}
+	}
+}
