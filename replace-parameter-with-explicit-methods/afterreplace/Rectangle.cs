d47bc6a7e4ff125a16d6bb156f80e4f83dab0481
Hoang Huynh Tuan Kiet <hoanghuynhtuankiet69@gmail.com>
2023-03-30 19:49:55 +0700

    Add replace parameter with explicit methods project

---
+++ replace-parameter-with-explicit-methods/afterreplace/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿namespace replace_parameter_with_explicit_methods.afterreplace
+{
+	internal class Rectangle
+	{
+		private int _width;
+		private int _height;
+
+		public Rectangle(int width, int height)
+		{
+			_width = width;
+			_height = height;
+		}
+
+		public void SetWidth(int width)
+		{
+			_width = width;
+		}
+
+		public void SetHeight(int height)
+		{
+			_height = height;
+		}
+	}
+}
---
+++ replace-parameter-with-explicit-methods/beforereplace/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿namespace replace_parameter_with_explicit_methods.beforereplace
+{
+	internal class Rectangle
+	{
+		private int _width;
+		private int _height;
+
+		public Rectangle(int width, int height)
+		{
+			_width = width;
+			_height = height;
+		}
+
+		public void SetValue(String attr, int value)
+		{
+			if (attr.Equals("height"))
+			{
+				_height = value;
+				return;
+			}
+
+			if (attr.Equals("width"))
+			{
+				_width = value;
+			}
+		}
+	}
+}
